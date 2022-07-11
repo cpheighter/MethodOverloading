7e1753bff414735692a9ad3cd5b1dc11c704f8a9
cpheighter <cheighterjr@gmail.com>
2022-07-11 15:44:40 -0400

    init

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool z)
+        {
+            var total = x + y;
+           
+            
+            
+                if (total == 1  && z == true)
+                {
+                    return $"{total} dollar";
+                }
+                else if (total > 1 && z == true)
+                {
+                    return $"{total} dollars";
+                }
+                 else
+                {
+                return "Is False";
+                }
+            
+        }
+        
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using MethodOverloading;
+
+Methods.Add(2, 2);
+Methods.Add(48.62m, 3.39m);
+Methods.Add(5, 87, true);
